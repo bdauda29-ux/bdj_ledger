2a740e17b8b04bd1603d08d07d0a3eb8d7b32361
bdj-ledger <you@example.com>
2026-03-07 16:47:49 +0100

    Import_flash_from_flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify, g, send_file, session
+from flask import Flask, render_template, request, redirect, url_for, jsonify, g, send_file, session, flash
 import json
 import sqlite3
 from datetime import datetime, timedelta
